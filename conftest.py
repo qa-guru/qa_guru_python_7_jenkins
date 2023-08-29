1dae4fb4b1489e2b97da5422a053086deed8cb0a
svasenkov <aanher@gmail.com>
2023-08-29 20:21:55 +0200

    added demoqa test

---
+++ conftest.py
@@ -0,0 +1,23 @@
+
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    browser_version = "100.0"
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+    browser.quit()
