0b71c7f31ee6e7fc935f829cf5e68c186c47fefb
svasenkov <aanher@gmail.com>
2023-08-29 20:57:53 +0200

    added allure attach

--- conftest.py
+++ conftest.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 
+from utils import attach
+
 @pytest.fixture(scope='function')
 def setup_browser(request):
     browser_version = "100.0"
@@ -24,4 +26,10 @@ def setup_browser(request):
 
     browser = Browser(Config(driver))
     yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
     browser.quit()
